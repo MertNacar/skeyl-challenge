a7cdb8ca5695a29ff6b8b7bace92c4bd89ddc53a
Mert Nacar <mertnacar2374@gmail.com>
2020-05-19 23:18:38 +0300

    done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
-
+import pet3 from './data/pet3.json'
 function App() {
+
+  const [searchValue, setSearchValue] = useState("")
+  const [textAreaValue, setTextAreaValue] = useState("")
+
+  function searchJson() {
+    if (searchValue === "") {
+      setTextAreaValue(JSON.stringify(pet3, null, 2))
+
+    } else {
+      let searchArr = searchValue.split(".")
+      let filteredJson = pet3
+      searchArr.forEach(item => {
+        if (!item.includes("[")) {
+          filteredJson = filteredJson[item]
+        } else {
+          let index = item[item.length - 2]
+          let key = item.slice(0, -3)
+          filteredJson = filteredJson[key][index]
+        }
+      })
+      setTextAreaValue(JSON.stringify(filteredJson, null, 2))
+    }
+  }
+
+  function onChangeInput(e) {
+    setSearchValue(e.currentTarget.value)
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="section1">
+        <input className="input" onChange={(e) => onChangeInput(e)} />
+        <button className="button" onClick={() => searchJson()}>run</button>
+      </div>
+      <div className="section2">
+        <textarea readOnly value={textAreaValue} cols="90" rows="45" className="text-area"></textarea>
+      </div>
     </div>
   );
 }
